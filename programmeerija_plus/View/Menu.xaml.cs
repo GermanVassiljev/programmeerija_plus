7c7349c20b533804cab86e232a8375f041a7aab2
opilane <opilane@HP-CZC2349HTZ>
2023-05-16 15:47:25 +0300

    2

--- programmeerija_plus/View/Menu.xaml.cs
+++ programmeerija_plus/View/Menu.xaml.cs
@@ -12,9 +12,53 @@ namespace programmeerija_plus.View
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Menu : ContentPage
     {
+        List<string> nuppid = new List<string> {"Plaanid", "Sätted" };
         public Menu()
         {
             InitializeComponent();
+            StackLayout layout = new StackLayout();
+            layout.BackgroundColor = Color.SkyBlue;
+
+            Label Tekst = new Label
+            {
+                HorizontalOptions = LayoutOptions.Center,
+                BackgroundColor = Color.MidnightBlue,
+                TextColor = Color.White,
+                FontSize = 26,
+                Text = "Programmeerija +",
+                WidthRequest = 400
+            };
+            layout.Children.Add(Tekst);
+            for (int i = 0; i < nuppid.Count; i++)
+            {
+                Button e = new Button
+                {
+                    HorizontalOptions = LayoutOptions.Center,
+                    BackgroundColor = Color.MidnightBlue,
+                    TextColor = Color.White,
+                    FontSize = 14,
+                    Text = nuppid[i],
+                    CharacterSpacing = 5,
+                    WidthRequest = 500,
+                    Margin = 10,
+                };
+                e.Clicked += E_Clicked;
+                layout.Children.Add(e);
+            }
+            Content= layout;
+        }
+
+        private async void E_Clicked(object sender, EventArgs e)
+        {
+            var nupp = (Button)sender;
+            if (nupp.Text == nuppid[0])
+            {
+                await Navigation.PushAsync(new NavigationPage(new Plaanid()));
+            }
+            else if (nupp.Text == nuppid[1])
+            {
+                await Navigation.PushAsync(new NavigationPage(new Satted()));
+            }
         }
     }
 }
---
+++ programmeerija_plus/View/Satted.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace programmeerija_plus.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Satted : ContentPage
+    {
+        public Satted()
+        {
+            InitializeComponent();
+        }
+    }
+}
